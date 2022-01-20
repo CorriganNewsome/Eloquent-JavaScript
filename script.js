968a1d9d14754d0be945dd84c4bf37044df1447a
Corrigan Newsome <corrigannewsome@gmail.com>
2022-01-19 17:27:21 -0700

    doing code challenge from javascript course

--- script.js
+++ script.js
@@ -308,8 +308,79 @@ document.querySelector(".button").addEventListener("click", () => {
   let inputValue = document.querySelector(".input").value;
   console.log(inputValue);
 });
-*/
+
 let colorChange = () => {
   var col = document.getElementById("content");
   col.style.color = "#FF0000";
 };
+*/
+
+"use strict";
+/*
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '4';
+
+document.querySelector('.guess').value = '3';
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+
+document.querySelector(".number").textContent = "?";
+
+//HANDLING CLICK EVENTS
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  //When there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+    //Sets background color element to green
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    //Increases the size of the secret number after winning
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Number too high!";
+      score--; //score = score - 1;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lose the game!";
+    }
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Number too low!";
+      score--; //score = score - 1;
+      document.querySelector(".score").textContent = score;
+      //When player loses
+    } else {
+      document.querySelector(".message").textContent = "You lose the game!";
+    }
+  }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector("again").addEventListener("click", () => {});

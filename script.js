e8a1423d01cd9d23e48b7cd140291d4b6ccfc494
Corrigan Newsome <corrigannewsome@gmail.com>
2022-01-25 14:44:36 -0700

    destructuring arrays

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
-"use strict";
 //SCOPING IN PRACTICE
 // function calcAge(birthYear) {
 //   const age = 2021 - birthYear;
@@ -130,3 +129,50 @@ corrigan.calcAge();
 
 //Deep Clone
 //Typically uses libraries to achieve this.
+
+//Array Destructuring
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+//How to destructure an array
+let [main, , secondary] = restaurant.categories;
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Recieve 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+//How to destructure an array within an array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
